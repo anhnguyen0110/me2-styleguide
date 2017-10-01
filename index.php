f54fa81ecf560f78dd667d3f9bae261b57089299
Anh Nguyen <a.nguyen0110@gmail.com>
2017-10-01 12:44:59 +1100

    added button

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="theme-color" content="#000000">
   <!--[if lt IE 9]><link rel="stylesheet" href="css/sg-style-old-ie.css"><![endif]-->
-
-  <!-- https://github.com/sindresorhus/github-markdown-css -->
+  <!-- Font awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/github-markdown.css">
   <link rel="stylesheet" href="css/sg-style.css">
 
